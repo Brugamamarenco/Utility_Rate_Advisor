4d273ef5b84eabb4e6da5598faacd953514651e0
Bryan Rugama <brugamamarenco@ucmerced.edu>
2026-02-28 13:47:48 -0800

    created react components containers to understand what components are needed

---
+++ main.jsx
@@ -0,0 +1,18 @@
+export function display(){
+    <div> 
+
+    </div>
+}
+
+export function actionLog(){
+    <div> 
+    </div>
+}
+
+export function action(item){
+    <div> 
+        <h1> {item.product} </h1>
+        <h3> {item.cost} </h3>
+    </div>
+}
+
