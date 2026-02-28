9417239d56fa0f710901c4df4711d9306757c7a5
Bryan Rugama <brugamamarenco@ucmerced.edu>
2026-02-28 13:54:23 -0800

    added function needed to render react components

---
+++ frontpage/main.jsx
@@ -0,0 +1,29 @@
+export function display(){
+    <div> 
+
+    </div>
+}
+
+export function actionLog(){
+    <div> 
+    </div>
+}
+
+//used for creating the item
+export function action(item){
+    return (
+    <div> 
+        <h1> {item.product} </h1>
+        <h3> {item.cost} </h3>
+    </div>
+    )
+}
+
+//function to render 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<Garage />
+);
+
+
+
